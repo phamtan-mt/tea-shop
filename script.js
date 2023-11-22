e18ead9655463e63f6b6477e0fedcc54542edf62
phamtan-mt <144121664+phamtan-mt@users.noreply.github.com>
2023-11-22 18:20:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function openLoginForm() {
+    document.getElementById('modal').style.display = 'flex';
+    document.querySelector('.auth-form--login').style.display = 'block';
+    document.querySelector('.auth-form--signup').style.display = 'none';
+  }
+  
+  function openSignupForm() {
+    document.getElementById('modal').style.display = 'flex';
+    document.querySelector('.auth-form--login').style.display = 'none';
+    document.querySelector('.auth-form--signup').style.display = 'block';
+  }
+  
+  function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.querySelector('.auth-form--login').style.display = 'none';
+    document.querySelector('.auth-form--signup').style.display = 'none';
+  }
+  
+  
+  function toggleForm() {
+    const loginForm = document.querySelector('.auth-form--login');
+    const signupForm = document.querySelector('.auth-form--signup');
+  
+    if (loginForm.style.display === 'block') {
+      loginForm.style.display = 'none';
+      signupForm.style.display = 'block';
+    } else {
+      loginForm.style.display = 'block';
+      signupForm.style.display = 'none';
+    }
+  }
+  
+  
+  
